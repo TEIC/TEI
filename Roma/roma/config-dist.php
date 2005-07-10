44b374e838891383d0e62223ab08ce7b7b8f0ae5
Sebastian Rahtz <sebastian.rahtz@it.ox.ac.uk>
2005-07-10 22:11:07 +0000

    clean up paths

--- Roma/roma/config-dist.php
+++ Roma/roma/config-dist.php
@@ -17,8 +17,8 @@ define( 'roma_pdflatex', 'pdflatex' );
 
 //#########################
 
+define( 'roma_xquery_server', 'http://localhost:8080/cocoon/Query/' );
 define( 'roma_temporaryFilesDir', '/tmp' );
-define( 'roma_schemaDir', 'http://localhost/schema/relaxng' );
 define( 'roma_StylesheetDir', 'http://localhost/stylesheet' );
 
 define( 'roma_localStylesheetDir', 'roma/stylesheets' );
@@ -26,12 +26,4 @@ define( 'romadom_templateDir', 'roma/templates' );
 define( 'roma_templateDir', 'roma/templates' );
 define( 'roma_ressource_path', 'roma/res' );
 
-//#########################
-// different_stylesheets
-//
-define( 'roma_styleheet_docHtml', roma_StylesheetDir . '/teic/teihtml-teic.xsl' );
-define( 'roma_styleheet_docPDF', roma_StylesheetDir . '/fo/tei.xsl' );
-define( 'roma_styleheet_docLatex', roma_StylesheetDir . '/latex/teilatex.xsl' );
-define( 'roma_customization_validator', roma_schemaDir . 'schema/relaxng/p5odds.rng' );
-define( 'roma_xquery_server', 'http://localhost:8080/cocoon/Query/' );
 </script>
--- Roma/roma/config.php
+++ Roma/roma/config.php
@@ -18,13 +18,11 @@ define( 'roma_pdflatex', 'pdflatex' );
 //#########################
 
 define( 'roma_temporaryFilesDir', '/tmp' );
-define( 'roma_schemaDir', 'http://localhost/schema' );
 define( 'roma_StylesheetDir', 'http://localhost/stylesheet' );
 
 define( 'roma_localStylesheetDir', 'roma/stylesheets' );
 define( 'romadom_templateDir', 'roma/templates' );
 define( 'roma_templateDir', 'roma/templates' );
 define( 'roma_ressource_path', 'roma/res' );
-define( 'roma_customization_validator', roma_schemaDir . 'schema/relaxng/p5odds.rng' );
 define( 'roma_xquery_server', 'http://localhost:8080/cocoon/Query/' );
 </script>
