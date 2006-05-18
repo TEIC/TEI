ae7880872d7fb088f7ff0a1bba240f15e3b3df53
Syd Bauman <s.bauman@neu.edu>
2006-05-18 22:08:33 +0000

    delete no longer needed branches; re-arrange tags/ directory

--- import/addcol.pl
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/perl -w
-use SOAP::Lite;
-die "Usage: $0 collection\n" unless @ARGV == 1;
-my $collection = $ARGV[0];
-my $service = SOAP::Lite->service("http://localhost:8080/exist/services/Admin?WSDL");
-my $session = $service->connect("admin", "");
-print "Creating collection $collection\n";
-$service->createCollection($session, $collection) || print "failed\n";
-$service->disconnect($session);
