d948a9d9db54bc2fb70ff230be54470bd796f7d0
Sebastian Rahtz <sebastian.rahtz@it.ox.ac.uk>
2007-06-15 11:54:21 +0000

    simplify clean routine

--- P5/Utilities/cleanrnc.pl
+++ P5/Utilities/cleanrnc.pl
@@ -3,18 +3,11 @@ $clean=0;
 while (<>) {
  if (/<pre/) { $clean=1; print; }
  elsif (/<\/pre/) { $clean=0; print; }
+ elsif (/<eg/) { $clean=1; print; }
+ elsif (/<\/eg/) { $clean=0; print; }
  elsif ($clean eq 0) { print; }
- elsif (/^namespace may /) { print; }
  elsif (/namespaces/) { print; }
  elsif (/namespace</) { print; }
- elsif (/^namespace by /) { print; }
- elsif (/^namespace apply /) { print; }
- elsif (/^namespace remains /) { print; }
- elsif (/^namespace in /) { print; }
- elsif (/^namespace /) {  }
- elsif (/^default namespace /) { }
- elsif (/<term>namespace/) {print; }
- elsif (/<gloss>namespace/) {print; }
  elsif (/>namespace/){
 	s/(.*>)namespace.*/$1/;
 	print;
