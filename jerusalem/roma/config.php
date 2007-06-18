bd720c122d055844895721f5f56762efdba64704
Ioan Bernevig <i.bernevig@GMAIL.COM>
2007-06-18 13:08:48 +0000

    fixing paths

--- jerusalem/roma/config.php
+++ jerusalem/roma/config.php
@@ -1,10 +1,9 @@
 <script language="php">
 //#########################
 // Programm calls
-//
+// paths solved
 // Defines how to call trang
-define( 'roma_trang', 'trang');
-//java -jar /home/ioan/bin/trang-20030619/trang.jar' );
+define( 'roma_trang', '/usr/bin/trang' );
 
 // Defines how to call fop
 define( 'roma_fop', 'fop' );
@@ -19,13 +18,13 @@ define( 'roma_pdflatex', 'pdflatex' );
 //#########################
 
 define( 'roma_temporaryFilesDir', '/tmp' );
-define( 'roma_StylesheetDir', '/usr/share/xml/tei/stylesheet' );
-//define( 'roma_xquery_server', 'http://tei.oucs.ox.ac.uk/Query/' );
-define( 'roma_xquery_server', 'http://localhost:8080/exist/Query/' );
+define( 'roma_StylesheetDir', 'http://www.tei-c.org/release/xml/tei/stylesheet' );
+define( 'roma_xquery_server', 'http://tei.oucs.ox.ac.uk/Query/' );
 define( 'roma_localStylesheetDir', 'roma/stylesheets' );
 define( 'roma_templateDir', 'roma/templates' );
 define( 'roma_resource_path', 'roma/res' );
-define ('roma_local_p5','/usr/share/xml/tei/odd/p5subset.xml');
-define ('roma_version','2.8');
-define ('roma_date','2007-03-10');
+define ('roma_local_p5','file:///tmp/p5subset.xml');
+define ('roma_version','2.12');
+define ('roma_date','2007-06-10');
 </script>
+ 
