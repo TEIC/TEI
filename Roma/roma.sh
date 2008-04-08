ba347f709b1410e358ea2157c913ba9c00e4d066
Sebastian Rahtz <sebastian.rahtz@it.ox.ac.uk>
2008-04-08 21:51:23 +0000

    support choosing from amongst multiple schemaSpec in an ODD file

--- Roma/roma.sh
+++ Roma/roma.sh
@@ -15,7 +15,8 @@ makeODD()
     then
 	xmllint --noent --xinclude $ODD \
 	    | xsltproc -o $RESULTS/$ODD.compiled \
-	    $LANGUAGE $DOCLANG --stringparam TEIC $TEIC \
+	    $SELECTEDSCHEMA $LANGUAGE $DOCLANG \
+	    --stringparam TEIC $TEIC \
 	    --stringparam TEISERVER $TEISERVER  \
 	    --stringparam localsource "$LOCAL"  \
 	    $DEBUG  $TEIXSLDIR/odds/odd2odd.xsl -
@@ -35,35 +36,39 @@ makeODD()
 makeRelax() 
 {
     echo "2. make RELAX NG from compiled ODD"
-    xsltproc $PATTERN $DEBUG $LANGUAGE $DOCLANG --stringparam TEIC $TEIC \
-             --stringparam outputDir $RESULTS       \
+    xsltproc $PATTERN $DEBUG $LANGUAGE $DOCLANG  $SELECTEDSCHEMA \
+	     --stringparam parameterize $parameterize \
+             --stringparam TEIC $TEIC \
+             --stringparam outputDir $RESULTS   \
              $TEIXSLDIR/odds/odd2relax.xsl $RESULTS/$ODD.compiled
     (cd $RESULTS; \
     echo "3. make RELAX NG compact from XML"; \
-    trang $N.rng $N.rnc  || die " trang conversion to RNC fails"; \
-    xmllint --format $N.rng > $$.xml; mv $$.xml $N.rng )
+    trang $schema.rng $schema.rnc  || die " trang conversion to RNC fails"; \
+    xmllint --format $schema.rng > $$.xml; mv $$.xml $schema.rng )
  }
 
 makeXSD()
 {
     echo "4. make XSD from RELAX NG"
     (cd $RESULTS; \
-    trang  -o disable-abstract-elements $N.rng $N.xsd || die " trang fails";\
-    test -f xml.xsd && perl -p -i -e 's+\"xml.xsd\"+\"http://www.w3.org/2004/10/xml.xsd\"+' $N.xsd)
+    trang  -o disable-abstract-elements $schema.rng $schema.xsd || die " trang fails";\
+    test -f xml.xsd && perl -p -i -e 's+\"xml.xsd\"+\"http://www.w3.org/2004/10/xml.xsd\"+' $schema.xsd)
 }
 
 makeDTD()
 {
     echo "5. make DTD from compiled ODD"
-    xsltproc  $DEBUG $LANGUAGE $DOCLANG --stringparam TEIC $TEIC \
+    xsltproc  $DEBUG $LANGUAGE $DOCLANG   $SELECTEDSCHEMA \
+	    --stringparam parameterize $parameterize \
+	    --stringparam TEIC $TEIC \
             --stringparam outputDir $RESULTS       \
             $TEIXSLDIR/odds/odd2dtd.xsl $RESULTS/$ODD.compiled
 }
 
 makeHTMLDOC() 
 {
-    echo "8. make HTML documentation $N.doc.html "
-    xsltproc 	-o $RESULTS/$N.doc.html \
+    echo "8. make HTML documentation $schema.doc.html "
+    xsltproc 	-o $RESULTS/$schema.doc.html \
 	$DEBUG  $LANGUAGE $DOCLANG --stringparam TEIC $TEIC \
 	--stringparam STDOUT true \
 	--stringparam splitLevel -1 \
@@ -72,20 +77,20 @@ makeHTMLDOC()
 
 makePDFDOC() 
 {
-    echo "7. make PDF documentation $N.doc.pdf and $N.doc.tex "
+    echo "7. make PDF documentation $schema.doc.pdf and $schema.doc.tex "
     xsltproc $DEBUG $LANGUAGE $DOCLANG --stringparam TEIC $TEIC \
-	-o $RESULTS/$N.doc.tex \
-	$TEIXSLDIR/latex/tei.xsl $N.doc.xml
-    pdflatex $N.doc.tex
+	-o $RESULTS/$schema.doc.tex \
+	$TEIXSLDIR/latex/tei.xsl $schema.doc.xml
+    pdflatex $schema.doc.tex
 }
 
 makeXMLDOC() 
 {
-    echo "6. make expanded documented ODD $N.doc.xml "
+    echo "6. make expanded documented ODD $schema.doc.xml "
     xsltproc $DEBUG $LANGUAGE $DOCLANG --stringparam TEISERVER $TEISERVER  \
 	--stringparam localsource "$LOCAL"  \
 	--stringparam TEIC $TEIC \
-	-o $RESULTS/$N.doc.xml \
+	-o $RESULTS/$schema.doc.xml \
 	$TEIXSLDIR/odds/odd2lite.xsl $RESULTS/$ODD.compiled 
 }
 
@@ -113,18 +118,20 @@ echo "  --xsl=$TEIXSLDIR"
 echo "  --teiserver=$TEISERVER"
 echo "  --localsource=$LOCALSOURCE  # local copy of P5 sources"
 echo "  options, binary switches:"
+echo "  --compile          # create compiled file odd"
+echo "  --debug            # leave temporary files, etc."
 echo "  --doc              # create expanded documented ODD (TEI Lite XML)"
-echo "  --lang=LANG        # language for names of attrbutes and elements"
-echo "  --doclang=LANG     # language for documentation"
 echo "  --dochtml          # create HTML version of doc"
-echo "  --patternprefix=STRING # prefix RELAX NG patterns with STRING"
+echo "  --doclang=LANG     # language for documentation"
 echo "  --docpdf           # create PDF version of doc"
+echo "  --lang=LANG        # language for names of attrbutes and elements"
 echo "  --nodtd            # suppress DTD creation"
 echo "  --norelax          # suppress RELAX NG creation"
-echo "  --noxsd            # suppress W3C XML Schema creation"
 echo "  --noteic           # suppress TEI-specific features"
-echo "  --debug            # leave temporary files, etc."
-echo "  --compile          # create compiled file odd"
+echo "  --noxsd            # suppress W3C XML Schema creation"
+echo "  --parameterize     # create parameterized DTD"
+echo "  --patternprefix=STRING # prefix RELAX NG patterns with STRING"
+echo "  --schema=NAME      # select name schema spec"
 exit 1
 }
 
@@ -145,25 +152,30 @@ xsd=true
 doc=false
 docpdf=false
 dochtml=false
+parameterize=false
+SELECTEDSCHEMA=
+schema=
 PATTERNPREFIX=
 while test $# -gt 0; do
   case $1 in
-    --xsl=*)       TEIXSLDIR=`echo $1 | sed 's/.*=//'`;;
-    --lang=*)      lang=`echo $1 | sed 's/.*=//'`;;
-    --doclang=*)   doclang=`echo $1 | sed 's/.*=//'`;;
-    --docflags=*)  DOCFLAGS=`echo $1 | sed 's/.*=//'`;;
+    --compile)     compile=true;dtd=false;xsd=false;doc=false;relax=false;;
+    --debug)       debug=true;;
     --doc)         doc=true;;
+    --docflags=*)  DOCFLAGS=`echo $1 | sed 's/.*=//'`;;
     --dochtml)     dochtml=true;;
+    --doclang=*)   doclang=`echo $1 | sed 's/.*=//'`;;
     --docpdf)      docpdf=true;;
-    --teiserver=*) TEISERVER=`echo $1 | sed 's/.*=//'`;;
+    --lang=*)      lang=`echo $1 | sed 's/.*=//'`;;
     --localsource=*) LOCALSOURCE=`echo $1 | sed 's/.*=//'`;;
     --nodtd)       dtd=false;;
     --norelax)     relax=false;;
-    --noxsd)       xsd=false;;
     --noteic)      TEIC=false;;
+    --noxsd)       xsd=false;;
+    --parameterize)       parameterize=true;;
+    --schema=*)    schema=`echo $1 | sed 's/.*=//'`;;
     --patternprefix=*) PATTERNPREFIX=`echo $1 | sed 's/.*=//'`;;
-    --debug)       debug=true;;
-    --compile)     compile=true;dtd=false;xsd=false;doc=false;relax=false;;
+    --teiserver=*) TEISERVER=`echo $1 | sed 's/.*=//'`;;
+    --xsl=*)       TEIXSLDIR=`echo $1 | sed 's/.*=//'`;;
     --help)        usageMsg;;
      *) if test "$1" = "${1#--}" ; then 
 	   break
@@ -189,12 +201,15 @@ echo "TEI stylesheet tree: $TEIXSLDIR"
 test -d $TEIXSLDIR/odds || \
      GET -e -d $TEIXSLDIR/odds/odd2odd.xsl > /dev/null || \
      die "stylesheet location $TEIXSLDIR is not accessible"
-N=$(xsltproc $TEIXSLDIR/odds/extract-schemaSpec-ident.xsl $1 | head -1)
-N=${N:?"Unable to ascertain ident= of <schemaSpec>"}
+if test "x$schema" = "x"
+then
+ schema=$(xsltproc $TEIXSLDIR/odds/extract-schemaSpec-ident.xsl $1 | head -1)
+ schema=${schema:?"Unable to ascertain ident= of <schemaSpec>"}
+fi
 echo "Results to: $RESULTS"
 mkdir -p $RESULTS || die "cannot make directory $RESULTS"
 D=`date "+%Y-%m-%d %H:%M:%S.%N"`
-echo "Process $ODD to create $N{.dtd|.xsd|.doc.xml|.rng|.rnc} in $RESULTS"
+echo "Process $ODD to create $schema{.dtd|.xsd|.doc.xml|.rng|.rnc} in $RESULTS"
 echo "========= $D Roma starts, execution:"
 if test "x$PATTERNPREFIX" = "x"
 then
@@ -215,6 +230,7 @@ else
     fi
 fi
 
+SELECTEDSCHEMA=" --stringparam selectedSchema $schema "
 
 if test "x$doclang" = "x"
 then
