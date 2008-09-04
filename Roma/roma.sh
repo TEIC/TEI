bf2c4c5de5f43a336a20a502863e7dd1e45ef092
Sebastian Rahtz <sebastian.rahtz@it.ox.ac.uk>
2008-09-04 10:07:02 +0000

    make sure teiHeader is copied into local source

--- Roma/roma.sh
+++ Roma/roma.sh
@@ -254,12 +254,12 @@ cat > subset.xsl <<EOF
 <xsl:stylesheet version="1.0"
   xmlns:tei="http://www.tei-c.org/ns/1.0"
   xmlns:xsl="http://www.w3.org/1999/XSL/Transform">
+  <xsl:key name="ALL" use="1" 
+    match="tei:elementSpec|tei:macroSpec|tei:classSpec|tei:moduleSpec"/>
   <xsl:template match="/">
     <tei:TEI>
-      <xsl:copy-of select=".//tei:elementSpec"/>
-      <xsl:copy-of select=".//tei:macroSpec"/>
-      <xsl:copy-of select=".//tei:classSpec"/>
-      <xsl:copy-of select=".//tei:moduleSpec"/>
+      <xsl:copy-of select="tei:TEI/tei:teiHeader"/>
+      <xsl:copy-of select="key('ALL',1)"/>
     </tei:TEI>
 </xsl:template>
 </xsl:stylesheet>
