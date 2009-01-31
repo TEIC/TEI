7d5830f365b011bd5bc4b13d89f16d221bdfac75
Sebastian Rahtz <sebastian.rahtz@it.ox.ac.uk>
2009-01-31 15:56:48 +0000

    release 3.8

--- Roma/roma/config-dist.php
+++ Roma/roma/config-dist.php
@@ -34,5 +34,5 @@ define ( 'roma_localStylesheetDir',  'roma/stylesheets' );
 define ( 'roma_templateDir',         'roma/templates' );
 define ( 'roma_resource_path',       'roma/res' );
 define ('roma_version','3.8');
-define ('roma_date','2009-01-24');
+define ('roma_date','2009-01-31');
 </script>
