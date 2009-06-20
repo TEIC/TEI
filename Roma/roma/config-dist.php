350263607031b880c394d6c9a392528b6f44414f
Sebastian Rahtz <sebastian.rahtz@it.ox.ac.uk>
2009-06-20 12:12:28 +0000

    get things in sync for release

--- Roma/roma/config-dist.php
+++ Roma/roma/config-dist.php
@@ -34,5 +34,5 @@ define ( 'roma_localStylesheetDir',  'roma/stylesheets' );
 define ( 'roma_templateDir',         'roma/templates' );
 define ( 'roma_resource_path',       'roma/res' );
 define ('roma_version','3.11');
-define ('roma_date','2009-05-04');
+define ('roma_date','2009-06-19');
 </script>
