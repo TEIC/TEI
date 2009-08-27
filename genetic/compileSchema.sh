691e8a0fc11b12d8a490277f43801fe82a93b880
Lou Burnard <lou.burnard@retired.ox.ac.uk>
2009-08-27 23:58:44 +0000

    this is going to be a long job

--- genetic/compileSchema.sh
+++ genetic/compileSchema.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
 
-roma --nodtd --noxsd geneticTEI.xml compiledSchema/
+#roma --nodtd --noxsd geneticTEI.xml compiledSchema/
+/usr/bin/roma2 --dochtml --docflags="summaryDoc=true" \
+  --nodtd --noxsd geneticTEI.xml compiledSchema/
+rnv compiledSchema/geneticTEI.rnc constr_example/transcript_D1.xml
