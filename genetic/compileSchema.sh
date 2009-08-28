188c95385150086e77515f5c83c7421bdbf3784c
Lou Burnard <lou.burnard@retired.ox.ac.uk>
2009-08-28 16:21:27 +0000

    more tweaks

--- genetic/compileSchema.sh
+++ genetic/compileSchema.sh
@@ -3,4 +3,5 @@
 #roma --nodtd --noxsd geneticTEI.xml compiledSchema/
 /usr/bin/roma2 --dochtml --docflags="summaryDoc=true" \
   --nodtd --noxsd geneticTEI.xml compiledSchema/
+rsync compiledSchema/geneticTEI.doc.html linux.ox.ac.uk:public_html/wip/
 rnv compiledSchema/geneticTEI.rnc constr_example/transcript_D1.xml
