86f4c001ba1d70049d96001a92aa18c20a8f93b1
Sebastian Rahtz <sebastian.rahtz@it.ox.ac.uk>
2011-11-05 23:47:51 +0000

    change licence

--- Roma/roma/roma.php
+++ Roma/roma/roma.php
@@ -9,24 +9,38 @@
 
 //This is the Roma processor
 //
-// This program is free software; you can redistribute it and/or modify
-// it under the terms of the GNU General Public License as published by
-// the Free Software Foundation; either version 2 of the License, or
-// (at your option) any later version.
-//
-// This Program is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; witout even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-// GNU General Public License for more details.
-//
-// You should have received a copy of the GNU General Public License
-// along with this program; if not, write to the Free Software
-// Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
-//
-// The author may be contacted via the e-mail address
-//
-// arno-oss@mittelbach-online.de
-
+// This software is dual-licensed:
+// 
+// 1. Distributed under a Creative Commons Attribution-ShareAlike 3.0
+// Unported License http://creativecommons.org/licenses/by-sa/3.0/ 
+// 
+// 2. http://www.opensource.org/licenses/BSD-2-Clause
+// 		
+// All rights reserved.
+// 
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are
+// met:
+// 
+// * Redistributions of source code must retain the above copyright
+// notice, this list of conditions and the following disclaimer.
+// 
+// * Redistributions in binary form must reproduce the above copyright
+// notice, this list of conditions and the following disclaimer in the
+// documentation and/or other materials provided with the distribution.
+// 
+// This software is provided by the copyright holders and contributors
+// "as is" and any express or implied warranties, including, but not
+// limited to, the implied warranties of merchantability and fitness for
+// a particular purpose are disclaimed. In no event shall the copyright
+// holder or contributors be liable for any direct, indirect, incidental,
+// special, exemplary, or consequential damages (including, but not
+// limited to, procurement of substitute goods or services; loss of use,
+// data, or profits; or business interruption) however caused and on any
+// theory of liability, whether in contract, strict liability, or tort
+// (including negligence or otherwise) arising in any way out of the use
+// of this software, even if advised of the possibility of such damage.
+// 
 // ######################################################################
 // --- The Roma PACKAGE
 // ######################################################################
