ac5d714a0bc24da7d79e78aaf4cbacf4501987d6
Sebastian Rahtz <sebastian.rahtz@it.ox.ac.uk>
2012-01-03 11:24:52 +0000

    make object work

---
+++ Documents/Object/ms.pl
@@ -0,0 +1,19 @@
+print "<listObject>\n";
+$n=0;
+while (<>) {
+chop;
+s/&/&amp;/g;
+ ($key,$rest) = /([A-Z]+) (.*)/;
+ if ($key eq 'NUMB') {
+   if ($n > 0) { print "</object>\n"; }
+   $n=$n+1;
+   $rest =~ s/ /_/g;
+   print qq(<object n="$n" xml:id="$rest">\n);
+ }
+ elsif ($key eq 'CONT') { print qq(<idno type="CONT">$rest</idno>\n); }
+ elsif ($key eq 'SMF') { print qq(<idno type="SMF">$rest</idno>\n); }
+ elsif ($key eq 'SQU') { print qq(<idno type="SQU">$rest</idno>\n); }
+ elsif ($key eq 'DESC') { print qq(<desc>$rest</desc>\n); }
+
+}
+print "</object></listObject>\n";
