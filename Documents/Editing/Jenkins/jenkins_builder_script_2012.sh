c1f57b6d4cc330958d027f088c6202c800aaac50
Martin Holmes <mholmes@uvic.ca>
2012-06-13 17:56:34 +0000

    Tweak to script.

--- Documents/Editing/Jenkins/jenkins_builder_script_2012.sh
+++ Documents/Editing/Jenkins/jenkins_builder_script_2012.sh
@@ -66,9 +66,6 @@ echo "Do you want to continue? Press return to continue,
 Control+c to stop."
 read
 
-#Set -e so we exit if something goes wrong. More useful error messages
-#would be helpful, in the future.
-set -e
 
 #Save the current directory so that we can come back here.
 currDir=`pwd`
@@ -123,6 +120,11 @@ echo ""
   echo "Press return to continue, or Control+c to stop."
   read
 fi
+
+
+#Set -e so we exit if something goes wrong. More useful error messages
+#would be helpful, in the future.
+set -e
 
 echo ""
 echo "*******************************************"
