bf9550167da4384ef77538b39c01e3c53d887ee1
Sebastian Rahtz <sebastian.rahtz@it.ox.ac.uk>
2012-06-20 09:37:05 +0000

    messages

--- P5/Utilities/tei-database-rebuild.sh
+++ P5/Utilities/tei-database-rebuild.sh
@@ -20,7 +20,10 @@ OPTS="--webapp=$WEBAPP --port=$PORT --host=localhost "
 echo Using $HOST:$PORT/$WEBAPP
 
 cd /tmp
+echo remove existing collection in eXist
 perl $X/addtoexist.pl $OPTS --remove=/db/TEI
 perl $X/addtoexist.pl $OPTS --remove=/db/system/config/db/TEI/index.xconf
+echo add config,  $X/index.xconf
 perl $X/addtoexist.pl $OPTS -c /db/system/config/db/TEI -s $X/index.xconf
+echo add TEI P5, $S/p5subset.xml
 perl $X/addtoexist.pl $OPTS -c /db/TEI -s $S/p5subset.xml
