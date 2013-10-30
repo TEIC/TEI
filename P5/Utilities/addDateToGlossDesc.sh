111e602b15ae015d4b72e723455c099f093c72d7
Sebastian Rahtz <sebastian.rahtz@it.ox.ac.uk>
2013-10-30 13:25:45 +0000

    script to add date to English descriptions

--- P5/Utilities/addDateToGlossDesc.sh
+++ P5/Utilities/addDateToGlossDesc.sh
@@ -2,8 +2,8 @@
 # run over all the *Spec files and add the date of the English version of gloss/desc/remarks
 for i in *.xml
 do
-    cp $i safe_$i
-    svn log $i | grep "^r[0-9]" | perl -p -e "s@([^\|]+)..[^\|]+..([^ |]+).*@svn up -\1 $i; saxon -o:new_$i safe_$i ../../Utilities/addDateToGlossDesc.xsl date=\2 file=$i;mv new_$i safe_$i@"
-    mv safe_$i $i
+    echo cp $i safe_$i
+    svn log $i | grep "^r[0-9]" | perl -p -e "s@([^\|]+)..[^\|]+..([^ |]+).*@svn up -\1 $i; xmllint --noout $i && saxon -o:new_$i safe_$i ../../Utilities/addDateToGlossDesc.xsl date=\2 file=$i;mv new_$i safe_$i@"
+    echo mv safe_$i $i
 done
 
