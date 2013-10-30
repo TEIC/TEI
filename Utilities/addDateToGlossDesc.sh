0b4c7f597c14407a5bfe10b6b910b78c53cb534c
Sebastian Rahtz <sebastian.rahtz@it.ox.ac.uk>
2013-10-30 15:41:14 +0000

    tiny bit of doc

--- Utilities/addDateToGlossDesc.sh
+++ Utilities/addDateToGlossDesc.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
-# run over all the *Spec files and add the date of the English version of gloss/desc/remarks
+# run over all the *Spec files and add the date of the English version of gloss/desc/remarks.
+# Get the SVN log, and parse it to create a shell script which
+# checks out each revision in turn, and compares that to the previous version.
+#
+# if the checked-out version isnt a well-formed XML file, dont even try the comparison.
 for i in *.xml
 do
     echo cp $i safe_$i
