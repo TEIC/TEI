02207212cc83af06451bd392de493f9d01e6f3b4
Peter Stadler <stadler@edirom.de>
2014-09-15 14:10:24 +0000

    replaced tei-database-rebuild.sh: now uses a simple REST call instead of the XMLRPC via perl

--- Utilities/tei-database-rebuild.sh
+++ Utilities/tei-database-rebuild.sh
@@ -1,29 +1,36 @@
 #!/bin/sh
 # Rebuild TEI eXist database
-# Sebastian Rahtz
-# 2005-04-22
+# Peter Stadler
+# 2014-09-15
 # License: GPL
 
-X=/usr/share/xml/tei/xquery
-S=/usr/share/xml/tei/odd
+LOCAL_PATH_P5SUBSET=/var/www/vhosts/tei-c.org/projects/tei/web/Vault/P5/current/xml/tei/odd/p5subset.xml
+REMOTE_PATH_P5SUBSET=localhost:8080/exist/rest/db/TEI/p5subset.xml
+LOCAL_PATH_INDEX_CONFIGURATION=/var/www/vhosts/tei-c.org/projects/tei/web/Vault/P5/current/xml/tei/xquery/index.xconf
+REMOTE_PATH_INDEX_CONFIGURATION=localhost:8080/exist/rest/db/system/config/db/TEI/index.xconf
+EXIST_USER=
+EXIST_USER_PASS=
 
-HOSTNAME=`hostname`
-HOST=localhost
-PORT=8080
-WEBAPP=exist
+# All the parameters above can and shall be overwritten by a config file
+CONFIG=~/.tei-exist.conf
 
-case $HOSTNAME in
-   s16382816.onlinehome-server.info) HOST=www.tei-c.org;PORT=8040;WEBAPP=exist;;
-   tei.oucs.ox.ac.uk) HOST=localhost;PORT=8080;WEBAPP=exist;;
-esac
-OPTS="--webapp=$WEBAPP --port=$PORT --host=localhost "
-echo Using port $PORT on $HOST, webapp name is $WEBAPP
+# Checking config with credentials …
+if [ -f $CONFIG ] ; then
+    source $CONFIG
+else
+    echo >&2 "Config file with credentials is missing"
+    echo >&2 "Moving on with hard coded values …"
+fi
 
-cd /tmp
-echo remove existing collection in eXist
-perl $X/addtoexist.pl $OPTS --remove=/db/TEI
-perl $X/addtoexist.pl $OPTS --remove=/db/system/config/db/TEI/index.xconf
-echo add config file  $X/index.xconf
-perl $X/addtoexist.pl $OPTS -c /db/system/config/db/TEI -s $X/index.xconf
-echo add TEI P5 file $S/p5subset.xml
-perl $X/addtoexist.pl $OPTS -c /db/TEI -s $S/p5subset.xml
+# Checking for curl …
+if [ ! `command -v curl 2>/dev/null` ] ; then 
+    echo >&2 "You do not have curl installed"  
+    echo >&2 "Dying …"
+    exit 1
+fi
+
+# First, upload the index configuration
+curl --user $EXIST_USER:$EXIST_USER_PASS -f --upload-file $LOCAL_PATH_INDEX_CONFIGURATION $REMOTE_PATH_INDEX_CONFIGURATION
+
+# Second, upload the new p5subset.xml
+curl --user $EXIST_USER:$EXIST_USER_PASS -f --upload-file $LOCAL_PATH_P5SUBSET $REMOTE_PATH_P5SUBSET
