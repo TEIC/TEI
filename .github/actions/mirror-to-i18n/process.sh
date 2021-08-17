c5928e042ecba8c1659cacafa0934213d1380827
Hugh Cayless <philomousos@gmail.com>
2021-08-17 16:36:13 -0400

    Wrong file.

--- .github/actions/mirror-to-i18n/process.sh
+++ .github/actions/mirror-to-i18n/process.sh
@@ -1,4 +1,4 @@
 git config --global user.email "$ACTION_USER_EMAIL"
 git config --global user.name "$ACTION_USER_NAME"
 git remote add i18n "https://x-access-token:$API_TOKEN@github.com/TEIC/I18n-TEI"
-git push i18n main
+git push i18n dev:main
