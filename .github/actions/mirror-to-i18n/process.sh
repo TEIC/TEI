c17165a182e2d492c5a160c20a481c9474168b55
Hugh Cayless <philomousos@gmail.com>
2021-08-18 08:11:36 -0400

    Trying a different URI.

--- .github/actions/mirror-to-i18n/process.sh
+++ .github/actions/mirror-to-i18n/process.sh
@@ -2,4 +2,4 @@
 
 git config --global user.email "$ACTION_USER_EMAIL"
 git config --global user.name "$ACTION_USER_NAME"
-git push "https://x-access-token:$API_TOKEN@github.com/TEIC/I18n-TEI" dev:main
+git push "https://tei-bot:$API_TOKEN@github.com/TEIC/I18n-TEI" dev:main
