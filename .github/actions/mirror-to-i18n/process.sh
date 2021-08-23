08303a4c8e9ab44c72749932d4507c533ec47328
Hugh Cayless <philomousos@gmail.com>
2021-08-23 12:25:41 -0400

    More debugging.

--- .github/actions/mirror-to-i18n/process.sh
+++ .github/actions/mirror-to-i18n/process.sh
@@ -6,6 +6,8 @@ git config --global user.name "$ACTION_USER_NAME"
 mkdir -p /root/.ssh
 echo "$SSH_PRIVATE_KEY" > /root/.ssh/id_rsa
 chmod 600 /root/.ssh/id_rsa
+mkdir -p ~/.ssh
+cp /root/.ssh/* ~/.ssh/ 2> /dev/null || true
 
 GIT_SSH_COMMAND="ssh -v"
 
