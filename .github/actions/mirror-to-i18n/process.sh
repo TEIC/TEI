de77041f7efe69ce2db14e841958017817d2bef8
Hugh Cayless <philomousos@gmail.com>
2021-08-23 10:52:50 -0400

    Trying one more time.

--- .github/actions/mirror-to-i18n/process.sh
+++ .github/actions/mirror-to-i18n/process.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-git config --global user.email "$ACTION_USER_EMAIL"
-git config --global user.name "$ACTION_USER_NAME"
+#git config --global user.email "$ACTION_USER_EMAIL"
+#git config --global user.name "$ACTION_USER_NAME"
 git push "https://tei-bot:$API_TOKEN@github.com/TEIC/I18n-TEI" dev:main
