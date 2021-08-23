fb655ecb66d25f34b0e3885a269369450d8fd4c2
Hugh Cayless <philomousos@gmail.com>
2021-08-23 11:16:34 -0400

    Trying a different way.

--- .github/actions/mirror-to-i18n/process.sh
+++ .github/actions/mirror-to-i18n/process.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
-#git config --global user.email "$ACTION_USER_EMAIL"
-#git config --global user.name "$ACTION_USER_NAME"
-git push "https://tei-bot:$API_TOKEN@github.com/TEIC/I18n-TEI" dev:main
+git config --global user.email "$ACTION_USER_EMAIL"
+git config --global user.name "$ACTION_USER_NAME"
+
+mkdir -p /root/.ssh
+echo "$SSH_PRIVATE_KEY" > /root/.ssh/id_rsa
+chmod 600 /root/.ssh/id_rsa
+
+SOURCE=`pwd`
+
+git clone git@github.com:TEIC/I18n-TEI.git /opt/I18n-TEI
+cd /opt/I18n-TEI
+git rebase $SOURCE dev
+git push origin main
