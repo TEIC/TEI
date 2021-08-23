edaf6c1eec35428ef909e1e01738153813684497
Hugh Cayless <philomousos@gmail.com>
2021-08-23 12:37:35 -0400

    Trying PAT again.

--- .github/actions/mirror-to-i18n/process.sh
+++ .github/actions/mirror-to-i18n/process.sh
@@ -3,16 +3,8 @@
 git config --global user.email "$ACTION_USER_EMAIL"
 git config --global user.name "$ACTION_USER_NAME"
 
-mkdir -p /root/.ssh
-echo "$SSH_PRIVATE_KEY" > /root/.ssh/id_rsa
-chmod 600 /root/.ssh/id_rsa
-mkdir -p ~/.ssh
-cp /root/.ssh/* ~/.ssh/ 2> /dev/null || true
-
-GIT_SSH_COMMAND="ssh -v"
-
 mkdir /opt/I18n-TEI
-git clone git@github.com:TEIC/I18n-TEI.git /opt/I18n-TEI
+git clone "https://tei-bot:$GITHUB_ACCESS_TOKEN@github.com/TEIC/I18n-TEI.git" /opt/I18n-TEI
 cd /opt/I18n-TEI
 git remote add upstream https://github.com/TEIC/TEI.git
 git pull --rebase upstream dev
